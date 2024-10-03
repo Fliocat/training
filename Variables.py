88e35987559131ad5589585cf49f59f97f3d0092
Иван <vanya56581@gmail.com>
2024-10-03 16:07:41 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,7 @@
+Number_of_HW = 12
+Number_of_hours = 1.5
+Course = 'Python'
+print ('Курс:', Course + ',',
+       'Всего задач:', str(Number_of_HW) + ",",
+       'Затрачено часов:', str(Number_of_hours) + ',',
+       'Среднеевремя выполненгия', str(Number_of_hours / Number_of_HW) + '.')

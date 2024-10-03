9763b2c7aec4f90bba35fae6b41ada5bef6c7586
Иван <vanya56581@gmail.com>
2024-10-03 17:22:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ print('2st program')
 print(9.99 > 9.98 and 100 != 1000.1)
 
 print('3st program')
+print(2 * 2 + 2)
+print(2 * (2 + 2))
 print(2 * 2 + 2 == 2 * (2 + 2))
 
 print('4st program')
 print(float('123.456') * 10)
 
-print(int(float('123.456') * 10))
+print(int(float('123.456') * 10) % 10)

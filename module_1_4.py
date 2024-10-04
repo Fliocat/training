b2a387f41cdfcb36f7895a9581019a58c28174eb
Иван <vanya56581@gmail.com>
2024-10-04 09:52:28 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите своё имя: ')
+print(len(my_string))
+print(f'Добро  пожаловать в мир, {my_string}' .upper())
+print(f'Добро  пожаловать в мир, {my_string}' .lower())
+print(f'Добро  пожаловать в мир, {my_string}' .replace(' ', ''))
+print(f'Добро  пожаловать в мир, {my_string}' [0])
+print(f'Добро  пожаловать в мир, {my_string}' [-1])

8c20a4ea85ef52ee882b3ed3dc7856f46917c0ac
Иван <vanya56581@gmail.com>
2024-10-07 12:59:08 +0300

    Update Basic data structures..py

--- Basic data structures..py
+++ Basic data structures..py
@@ -1,5 +1,3 @@
-from os.path import split
-
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
@@ -13,12 +11,10 @@ p_4 = sum(grades[4]) / len(grades[4])
 
 p = dict()
 
-p0 = dict(key=i[0] , values = p_0)
-p1 = dict(key=i[1] , values = p_1)
-p2 = dict(key=i[2] , values = p_2)
-p3 = dict(key=i[3] , values = p_3)
-p4 = dict(key=i[4] , values = p_4)
-
-p = p0, p1, p2, p3, p4
+p.setdefault(i[0], p_0)
+p.setdefault(i[1], p_1)
+p.setdefault(i[2], p_2)
+p.setdefault(i[3], p_3)
+p.setdefault(i[4], p_4)
 
 print(p)
